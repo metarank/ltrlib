53501f66f3033c53b616f556d456b696f5b384b3
Scala Steward <me@scala-steward.org>
2021-09-25 19:24:40 +0200

    Reformat with scalafmt 3.0.5

--- src/main/scala/io/github/metarank/ltrlib/metric/Metric.scala
+++ src/main/scala/io/github/metarank/ltrlib/metric/Metric.scala
@@ -3,8 +3,10 @@ package io.github.metarank.ltrlib.metric
 trait Metric {
 
   /** Eval metric for multiple query groups
-    * @param y ground truth
-    * @param yhat predicted values
+    * @param y
+    *   ground truth
+    * @param yhat
+    *   predicted values
     * @return
     */
   def eval(y: Array[Array[Double]], yhat: Array[Array[Double]]): Double
