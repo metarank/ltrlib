d75f6301b9886175d09cc3df6974552e178d9ee3
Scala Steward <scala_steward@virtuslab.com>
2023-02-24 09:47:45 +0000

    Reformat with scalafmt 3.7.2
    Executed command: scalafmt --non-interactive

--- src/main/scala/io/github/metarank/ltrlib/booster/LightGBMOptions.scala
+++ src/main/scala/io/github/metarank/ltrlib/booster/LightGBMOptions.scala
@@ -4,12 +4,12 @@ import io.github.metarank.ltrlib.booster.Booster.BoosterOptions
 import scala.util.Random
 
 case class LightGBMOptions(
-                            trees: Int = 100,
-                            learningRate: Double = 0.1,
-                            ndcgCutoff: Int = 10,
-                            maxDepth: Int = 8,
-                            randomSeed: Int = math.abs(Random.nextInt()),
-                            numLeaves: Int = 16,
-                            featureFraction: Double = 1.0,
-                            earlyStopping: Option[Int] = None
+    trees: Int = 100,
+    learningRate: Double = 0.1,
+    ndcgCutoff: Int = 10,
+    maxDepth: Int = 8,
+    randomSeed: Int = math.abs(Random.nextInt()),
+    numLeaves: Int = 16,
+    featureFraction: Double = 1.0,
+    earlyStopping: Option[Int] = None
 ) extends BoosterOptions
