fcff68a7e76d82a52aeac420e20130267d38624d
Scala Steward <scala_steward@virtuslab.com>
2023-09-07 00:45:25 +0000

    Update slf4j-api, slf4j-simple to 2.0.9

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,6 +1,6 @@
 object Deps {
   lazy val scalatestVersion = "3.2.15"
   lazy val ejmlVersion      = "0.40"
-  lazy val slf4jversion     = "2.0.7"
+  lazy val slf4jversion     = "2.0.9"
   lazy val circeVersion     = "0.14.3"
 }
