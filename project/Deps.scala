2e804bd7ead245c163ed7be67fb3c801a3375ba4
Scala Steward <scala_steward@virtuslab.com>
2023-09-12 21:03:42 +0000

    Update scalatest, scalatest-propspec to 3.2.17

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,5 +1,5 @@
 object Deps {
-  lazy val scalatestVersion = "3.2.15"
+  lazy val scalatestVersion = "3.2.17"
   lazy val ejmlVersion      = "0.40"
   lazy val slf4jversion     = "2.0.7"
   lazy val circeVersion     = "0.14.3"
